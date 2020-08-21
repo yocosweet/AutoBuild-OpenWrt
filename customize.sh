0c8afdf6154b1f09438b1668912fe14a48e373ad
yocosweet <40546262+yocosweet@users.noreply.github.com>
2020-08-21 14:45:13 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -18,6 +18,9 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-them
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
 # Add smartdns
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/smartdns ../smartdns
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
